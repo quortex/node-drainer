47200f8537b5f0b81749d56e8bd0f17c2d3d993f
Vincent Marguerie <v.marguerie@quortex.io>
2021-05-19 18:49:39 +0200

    deduce in / out clusterConfig from kubeconfig flag
    Co-authored-by: Antonin <9219052+antonincms@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"os"
 	"path/filepath"
@@ -17,23 +18,24 @@ import (
 	"quortex.io/node-drainer/pkg/logger"
 )
 
-var fEnableDevLogs bool
-var fLogVerbosity int
-var fInCluster bool
-var fSelector map[string]string
-var fEvictionGlobalTimeout int
-var fOlderThan time.Duration
-var fCount int
-
 func main() {
+	var (
+		fEnableDevLogs         bool
+		fLogVerbosity          int
+		fSelector              map[string]string
+		fEvictionGlobalTimeout int
+		fOlderThan             time.Duration
+		fCount                 int
+		fKubeConfig            string
+	)
 
 	flag.BoolVar(&fEnableDevLogs, "dev", false, "Enable dev mode for logging.")
 	flag.IntVar(&fLogVerbosity, "v", 3, "Logs verbosity. 0 => panic, 1 => error, 2 => warning, 3 => info, 4 => debug")
-	flag.BoolVar(&fInCluster, "incluster", true, "Wether this application runs in a Pod in the cluster.")
 	flag.Var(cliflag.NewMapStringString(&fSelector), "l", "Selector to list the nodes to drain.")
 	flag.IntVar(&fEvictionGlobalTimeout, "eviction-timeout", 300, "The timeout in seconds for pods eviction on Instance deletion.")
 	flag.DurationVar(&fOlderThan, "older-than", time.Hour*8, "The minimum lifespan that a node must have to be drained.")
 	flag.IntVar(&fCount, "count", 1, "The number of nodes to drain.")
+	flag.StringVar(&fKubeConfig, "kubeconfig", "", "(optional) absolute path to the kubeconfig file")
 	flag.Parse()
 
 	// Init logger
@@ -41,22 +43,18 @@ func main() {
 
 	var config *rest.Config
 	var err error
-	if fInCluster {
-		// InClusterConfig returns a config object which uses the service account
-		// kubernetes gives to pods. It's intended for clients that expect to be
-		// running inside a pod running on kubernetes. It will return ErrNotInCluster
-		// if called from a process not running in a kubernetes environment.
-		config, err = rest.InClusterConfig()
+
+	if fKubeConfig != "" {
+		// KubeConfig was set, use the provided kubeconfig
+		log.Info("Try to configure k8s client from provided kubeconfig")
+		config, err = clientcmd.BuildConfigFromFlags("", fKubeConfig)
 	} else {
-		var kubeconfig *string
-		if home := homedir.HomeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		// Else try to use kubernetes configuration
+		config, err = rest.InClusterConfig()
+		if homedir.HomeDir() != "" && errors.Is(err, rest.ErrNotInCluster) {
+			// Else if we are in a homedir, try to use ~/.kube/config
+			config, err = clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
 		}
-		flag.Parse()
-		// use the current context in kubeconfig
-		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	}
 	if err != nil {
 		log.Error(err, "Failed to init kubernetes rest config")
